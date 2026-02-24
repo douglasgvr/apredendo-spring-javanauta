665b48f2b2084b188355225f795bdb694cb1d773
douglasgvr <douglasfarmaangra@gmail.com>
2026-02-23 22:19:51 -0300

    feat: add `UsuarioController` with a POST endpoint to save new users.

--- src/main/java/apredendo_spring/controller/UsuarioController.java
+++ src/main/java/apredendo_spring/controller/UsuarioController.java
@@ -1,4 +1,22 @@
 package apredendo_spring.controller;
 
+import apredendo_spring.business.UsuarioService;
+import apredendo_spring.infastructure.entity.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public Usuario salvarUsuario(Usuario usuario) {
+       return usuarioService.salvaUsuario(usuario);
+    }
 }
